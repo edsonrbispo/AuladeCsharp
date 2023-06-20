a45022d82a390c499b91827a56ea2789554bd4b7
Edson Rodrigues <edsonrb.2008@gmail.com>
2023-06-20 14:53:07 -0300

    Primeiro Commit

---
+++ AplicativoTeste/AplicativoTeste/Form1.cs
@@ -0,0 +1,10 @@
+namespace AplicativoTeste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalculodeMedia/CalculodeMedia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculodeMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1;-
+            double nota2 = 10
+            double media;
+
+            nota1 = Convert.ToDouble(Console.ReadLine()); 
+            nota2 = double.Parse(Console.ReadLine()); 
+            
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine(media.ToString());
+
+        }
+
+    }
+}
